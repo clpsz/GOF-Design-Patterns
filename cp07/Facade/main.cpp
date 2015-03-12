0427ba6257bb5f7a252070c2b951c90f94982893
Zuo Pengpeng <zuopengpeng@uhappybox.cn>
2015-03-12 14:53:07 +0800

    add facade pattern example in chapter 7

--- cp07/Facade/main.cpp
+++ cp07/Facade/main.cpp
@@ -7,10 +7,15 @@ int main()
 {
     Screen *screen = new Screen();
     DvdPlayer *dp = new DvdPlayer();
-
     HomeTheaterFacade *htf = new HomeTheaterFacade(dp, screen);
+
     htf->watchMovie();
     htf->endMovie();
+
+    delete htf;
+    delete dp;
+    delete screen;
+
     return 0;
 }
 
